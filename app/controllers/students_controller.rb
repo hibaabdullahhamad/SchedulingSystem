dee4a173cdee0bb4be7beaff8059b717994b7483
Hiba Hamad <hibahamad@Hibas-MacBook-Air.local>
2025-01-22 00:01:33 +0300

    added controller actions for student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,18 @@
 class StudentsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
+    def create
+        student = Student.new(student_params)
+        if student.save
+          render json: { message: 'Student created successfully', student: student }, status: :created
+        else
+          render json: { errors: student.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name, :time_zone)
+    end
 end
